7ab394676993b9bd9013a51a52ada72f99bb0b53
HectorShaw <ogawde7@gmail.com>
2025-02-06 00:50:32 +0530

    feat: Add p2p transfers

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -12,6 +12,7 @@ export default function Layout({
                 <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
                 <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
                 <SidebarItem href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
+                <SidebarItem href={"/p2p"} icon={<Pay />} title="Pay" />
             </div>
         </div>
             {children}
@@ -29,6 +30,12 @@ function TransferIcon() {
     return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
   </svg>
+}
+
+function Pay(){
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
 }
 
 function TransactionsIcon() {
---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,9 @@
+import { SendCard } from "../../../components/SendCart";
+
+export default function() {
+  return (
+    <div className="w-full">
+      <SendCard />
+    </div>
+  );
+}
